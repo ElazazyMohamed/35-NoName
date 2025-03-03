0f8691fe087a286c590d97159adb48f21df9de2c
Mohamed <mohamed.mohsen1701@gmail.com>
2025-03-03 18:47:48 +0200

    I fixed the testDeleteOrderByIdEndPoint() case

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -35,13 +35,14 @@ public class OrderController {
 
     @DeleteMapping("/delete/{orderId}")
     public String deleteOrderById(@PathVariable UUID orderId){
-        try {
-            orderService.deleteOrderById(orderId);
-            return "Order deleted successfully";
-        }catch (IllegalArgumentException e){
+        Order order = orderService.getOrderById(orderId);
+        if (order == null) {
             return "Order not found";
         }
+        orderService.deleteOrderById(orderId);
+        return "Order deleted successfully";
     }
+
 
 
 }
